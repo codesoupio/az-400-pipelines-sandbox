62950ddd53c3a5cc7feb578ab466e1135043c3eb
Vassilis Kotsaris <vkotsaris@gmail.com>
2024-02-20 09:57:04 +0100

    Test env variables

--- src/main.ts
+++ src/main.ts
@@ -1,12 +1,14 @@
-import express, {Express, Request, Response} from "express";
+import express, { Express, Request, Response } from 'express';
 
-const app: Express = express()
+const app: Express = express();
 const port = process.env.PORT ?? 4000;
+const testEnvVariable = process.env.TEST || 'default value';
 
-app.get("/", (req: Request, res: Response) => {
-    res.send("Hello AZ-400")
-})
+app.get('/', (req: Request, res: Response) => {
+  res.send(`Hello AZ-400`);
+});
 
 app.listen(port, () => {
-    console.log(`Server is running on port ${port}`)
-})
+  console.log(`Server is running on port ${port}`);
+  console.log(`Test env variable: ${testEnvVariable}`);
+});
